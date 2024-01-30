24719cb3780df33366382bfc5de5eec44f4ca73d
Roman Bogdanovskiy <r.bogdanovskiy@onpoint.ru>
2024-01-30 19:53:52 +0300

    Updates

---
+++ js/subscribe.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";var e;(e=document.querySelector("#subscribe-form")).querySelector("#subscribe-btn").addEventListener("click",(function(){e.reset()}))})();
+//# sourceMappingURL=subscribe.js.map
